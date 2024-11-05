24519fdfcaa43333e7b11d782e380d342ffe0889
Rubick1337 <poss1337@gmail.com.>
2024-11-05 10:51:12 +0300

    fix: columns in database

---
+++ Car_oop/Migrations/20241105074940_FixColumns.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Car_oop.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Cars_CarId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "mileage",
+                table: "ModelCar");
+
+            migrationBuilder.AddColumn<double>(
+                name: "payday",
+                table: "Persons",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CarId",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Persons",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "payday",
+                value: 1000.0);
+
+            migrationBuilder.UpdateData(
+                table: "Persons",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "payday",
+                value: 2000.0);
+
+            migrationBuilder.UpdateData(
+                table: "Persons",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "payday",
+                value: 3000.0);
+
+            migrationBuilder.UpdateData(
+                table: "Persons",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "payday",
+                value: 4000.0);
+
+            migrationBuilder.UpdateData(
+                table: "Persons",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "payday",
+                value: 1000.0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Cars_CarId",
+                table: "Orders",
+                column: "CarId",
+                principalTable: "Cars",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Cars_CarId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "payday",
+                table: "Persons");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CarId",
+                table: "Orders",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "mileage",
+                table: "ModelCar",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "ModelCar",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "mileage",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "ModelCar",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "mileage",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "ModelCar",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "mileage",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "ModelCar",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "mileage",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "ModelCar",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "mileage",
+                value: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Cars_CarId",
+                table: "Orders",
+                column: "CarId",
+                principalTable: "Cars",
+                principalColumn: "Id");
+        }
+    }
+}
--- Car_oop/Repository/Config/PersonConfiguration.cs
+++ Car_oop/Repository/Config/PersonConfiguration.cs
@@ -7,11 +7,11 @@ public class PersonConfiguration : IEntityTypeConfiguration<Person>
     public void Configure(EntityTypeBuilder<Person> builder)
     {
         builder.HasData(
-            new Person { Id = 1, experience = 5, name = "Alice", surname = "Johnson", PostId = 1 },
-            new Person { Id = 2, experience = 3, name = "Bob", surname = "Brown", PostId = 2 },
-            new Person { Id = 3, experience = 10, name = "Charlie", surname = "Davis", PostId = 3 },
-            new Person { Id = 4, experience = 2, name = "Diana", surname = "Miller", PostId = 4 },
-            new Person { Id = 5, experience = 1, name = "Edward", surname = "Wilson", PostId = 5 }
+            new Person { Id = 1, experience = 5, name = "Alice", surname = "Johnson", PostId = 1, payday = 1000 },
+            new Person { Id = 2, experience = 3, name = "Bob", surname = "Brown", PostId = 2, payday = 2000 },
+            new Person { Id = 3, experience = 10, name = "Charlie", surname = "Davis", PostId = 3,payday=3000 },
+            new Person { Id = 4, experience = 2, name = "Diana", surname = "Miller", PostId = 4, payday = 4000 },
+            new Person { Id = 5, experience = 1, name = "Edward", surname = "Wilson", PostId = 5,payday = 1000 }
         );
 
     }
